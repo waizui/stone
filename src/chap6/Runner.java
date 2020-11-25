c93ff61edcefbec8dd1171eee1043a62c16b2a92
leo <lch01234@gmail.com>
2020-11-25 22:39:05 +0800

    vv

--- src/chap6/Runner.java
+++ src/chap6/Runner.java
@@ -2,7 +2,9 @@ package chap6;
 import javassist.gluonj.util.Loader;
 
 public class Runner {
+
     public static void main(String[] args) throws Throwable {
         Loader.run(BasicInterpreter.class, args, BasicEvaluator.class);
     }
+    
 }
