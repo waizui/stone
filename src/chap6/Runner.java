17eacf1c93d47d165ea53ec82551fba48fbfa9dd
leo <lch01234@gmail.com>
2020-11-25 22:41:35 +0800

    test

--- src/chap6/Runner.java
+++ src/chap6/Runner.java
@@ -4,7 +4,9 @@ import javassist.gluonj.util.Loader;
 public class Runner {
 
     public static void main(String[] args) throws Throwable {
+
         Loader.run(BasicInterpreter.class, args, BasicEvaluator.class);
+
     }
 
 }
